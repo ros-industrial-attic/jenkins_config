09a7cca5b6016a241c6c115ee91a870d7d3587c3
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2013-07-02 10:14:38 -0500

    added list of tracked repositories and updated 'run_workspace_setup.bash' script accordingly

--- jobs/ros_industrial_groovy_git/scripts/run_workspace_setup.bash
+++ jobs/ros_industrial_groovy_git/scripts/run_workspace_setup.bash
@@ -4,6 +4,7 @@ source /opt/ros/groovy/setup.bash
 
 # checking if catkin ws is already initialized
 cd $WORKSPACE # entering job workspace
+JOB_SCRIPTS_PATH=${JENKINS_HOME}/jobs/${JOB_NAME}/scripts
 SETUP_FILE=$WORKSPACE"/devel/setup.bash"
 if [ -f $SETUP_FILE ];
 then
@@ -29,8 +30,10 @@ else
 	catkin_make
 
 	# setting up .rosinstall file
+	#rosws set ../src/industrial_core/ --git https://github.com/ros-industrial/industrial_core.git -y
+	
 	cd "$WORKSPACE/devel"
-	rosws set ../src/industrial_core/ --git https://github.com/ros-industrial/industrial_core.git -y
+	cat "$JOB_SCRIPTS_PATH/repository_list.txt" | awk -F',' '{system("rosws set "$1" --git "$2" -y"); print "added repository ", $2, " in local path ", $1}'
 
 	#sourcing setup file
 	source $SETUP_FILE	
