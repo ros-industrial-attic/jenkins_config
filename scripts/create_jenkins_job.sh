ecb3e4f4b1b4278774911e9189e5261fa5a3cd1f
jrgnicho <jrgnicho@gmail.com>
2013-07-17 15:42:35 -0500

    script for automating creation of jenkins jobs

---
+++ scripts/create_jenkins_job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# variables
+JOB_NAME=$1
+JENKINS_HOME=/var/lib/jenkins
+SCRIPTS_PATH=${JENKINS_HOME}/jenkins_config/scripts
+CONFIG_FILE_PATH=${JENKINS_HOME}/jenkins_config/jobs/template_job/config.xml
+JOB_PATH=${JENKINS_HOME}/jobs/${JOB_NAME}
+
+# creating job directory
+if [ ! -d ${JOB_PATH} ];
+then
+	mkdir ${JOB_PATH}
+	cp ${CONFIG_FILE_PATH} ${JOB_PATH}
+
+	# reloading configuration
+	wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+	java -jar jenkins-cli.jar -s http://localhost:8080 reload-configuration
+
+	echo "Created new jenkins job $JOB_NAME"
+fi
+
+
