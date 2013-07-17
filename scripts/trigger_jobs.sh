7dfeda566c0e2887be81961886a18230dcda2ea2
jrgnicho <jrgnicho@gmail.com>
2013-07-17 15:46:01 -0500

    iterates through workspace files and builds new jobs before checking workspaces

--- scripts/trigger_jobs.sh
+++ scripts/trigger_jobs.sh
@@ -8,19 +8,31 @@ SCRIPTS_PATH=${JENKINS_HOME}/jenkins_config/scripts
 echo Running trigger jenkins cron job ...
 cd ${JENKINS_HOME}/jenkins_config && git pull &>/dev/null
 
-for D in /var/lib/jenkins/jobs/*; do
+# creating jobs
+for F in ${JENKINS_HOME}/jenkins_config/workspaces/*; do
+	
+	file_name=${F##*/}
+	job_name=${F%.rosinstall}
+	job_name=${job_name##*/}
+	echo "found job $job_name with file $file_name"
+	${SCRIPTS_PATH}/create_jenkins_job.sh $job_name
+done
 
-echo "job found in directory $D"
-  crumbs=(${D//\// })                                                 
-  job=${crumbs[${#crumbs[@]} - 1]}
-  status=`/var/lib/jenkins/jenkins_config/scripts/check_workspace_status.sh $D/workspace $job`
-  if [ $status == "behind" ];
-  then
-    echo Triggering $job
-    chmod a+rwx -R $D/workspace
-    wget http://localhost:8080/job/$job/build?delay=0sec #&>/dev/null
-  else
-    echo $job already up to date.
-  fi
+
+# checking jobs
+for D in ${JENKINS_HOME}/jobs/*; do
+
+	echo "job found in directory $D"
+	  crumbs=(${D//\// })                                                 
+	  job=${crumbs[${#crumbs[@]} - 1]}
+	  status=`$SCRIPTS_PATH/check_workspace_status.sh $D/workspace $job`
+	  if [ $status == "behind" ];
+	  then
+	    echo Triggering $job
+	    chmod a+rwx -R $D/workspace
+	    wget http://localhost:8080/job/$job/build?delay=0sec #&>/dev/null
+	  else
+	    echo $job already up to date.
+	  fi
 done
 
