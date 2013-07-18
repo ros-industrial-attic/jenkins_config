37b7af0182779ea53544ba75c7011421d54d710c
jrgnicho <jrgnicho@gmail.com>
2013-07-18 11:54:42 -0500

    merges .rosinstall file for job in case a repo was added

--- scripts/build_catkin_workspace.sh
+++ scripts/build_catkin_workspace.sh
@@ -4,13 +4,20 @@ source /opt/ros/groovy/setup.bash
 
 # variables
 SETUP_FILE=$WORKSPACE"/setup.bash"
+ROS_INSTALL_FILE=${JENKINS_HOME}/jenkins_config/workspaces/${JOB_NAME}.rosinstall
+
+# merging workspace file
+echo "merging workspace file $ROS_INSTALL_FILE"
+cd ${WORKSPACE}
+rosws merge ${ROS_INSTALL_FILE} -r -y
 
 #sourcing setup file
+echo "sourcing setup"
 source $SETUP_FILE
 
 #updating repositories
 cd $WORKSPACE
-rosws update -v
+rosws update --delete-changed-uris -v
 
 echo "============ Building catkin workspace ============"
 catkin_make clean
