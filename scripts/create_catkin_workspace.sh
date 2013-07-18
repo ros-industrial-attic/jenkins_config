eff1cb8ca6b5ac86062866736280b9e6fa51e31e
jrgnicho <jrgnicho@gmail.com>
2013-07-18 11:55:59 -0500

    does not changed mode of repo files since this causes errors during a git pull

--- scripts/create_catkin_workspace.sh
+++ scripts/create_catkin_workspace.sh
@@ -9,7 +9,6 @@ JOB_WORKSPACE_FILE=$2
 #echo "============ Initializing and building catking workspace =============" >&1
 
 # initializing ros workspace 
-#echo "creating workspace in path: $WORKSPACE" >&1
 mkdir -p "$WORKSPACE/src" # creating source directory for catkin workspace
 cd $WORKSPACE # entering job workspace
 capture_stdout=$(rosws init ${WORKSPACE})
@@ -29,6 +28,4 @@ capture_stdout=$(rosws merge ${JOB_WORKSPACE_FILE} -y )
 # cloning all git repositories in src directory
 capture_stdout=$(rosws update -v )
 
-# changing mode of workpace
-chmod a+rwx -R ${WORKSPACE}
 
