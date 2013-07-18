4492f5c5bb49c7ec835c7fe8ed7c8bd7c60ae70f
jrgnicho <jrgnicho@gmail.com>
2013-07-18 11:59:19 -0500

    references local jenkins-cli.jar, ignores temporary .rosinstall files and does not change mode of git repo files in a job's workpace

--- scripts/trigger_jobs.sh
+++ scripts/trigger_jobs.sh
@@ -7,19 +7,26 @@ SCRIPTS_PATH=${JENKINS_HOME}/jenkins_config/scripts
 JENKINS_CLI=${JENKINS_HOME}/jenkins-cli.jar
 
 echo "updating jenkins_config repository"
-cd ${JENKINS_HOME}/jenkins_config && git pull &>/dev/null
+cd ${JENKINS_HOME}/jenkins_config && git pull #&>/dev/null
 
 if [ ! -f $JENKINS_CLI ];
 then
 	echo "downloading jenkins-cli.jar"
 	cd ${JENKINS_HOME}
 	wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+	chmod a+rwx ${JENKINS_CLI}
 fi
 
 
 # creating jobs
 for F in ${JENKINS_HOME}/jenkins_config/workspaces/*; do
 	
+	if [ "${F##*~}" == "" ];
+	then
+		echo "temp file found: $F, skipping"
+		continue
+	fi
+
 	file_name=${F##*/}
 	job_name=${F%.rosinstall}
 	job_name=${job_name##*/}
@@ -37,10 +44,11 @@ for D in ${JENKINS_HOME}/jobs/*; do
 	  status=`$SCRIPTS_PATH/check_workspace_status.sh $D/workspace $job`
 	  if [ $status == "behind" ];
 	  then
+
 	    echo Triggering $job
-	    chmod a+rwx -R $D/workspace
 	    #wget http://localhost:8080/job/$job/build?delay=0sec #&>/dev/null
-	    java -jar jenkins-cli.jar -s http://localhost:8080 build $job
+	    java -jar ${JENKINS_CLI} -s http://localhost:8080 build $job
+
 	  else
 	    echo $job already up to date.
 	  fi
