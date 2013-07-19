7b502fe3fdb5c18d1a907e3e397aa72c9ce3abc5
jrgnicho <jrgnicho@gmail.com>
2013-07-19 17:00:27 -0500

    merges workspace file before it checks revision status

--- scripts/check_workspace_status.sh
+++ scripts/check_workspace_status.sh
@@ -11,21 +11,37 @@ CATKIN_SETUP_FILE=${CATKIN_WORKSPACE}/devel/setup.bash
 # check if setup file for catkin workspace exists
 if [ -f $CATKIN_SETUP_FILE ];
 then
-
+	# entering workspace
 	cd $1
+
+	# merging workspace file
+	if [ -f $JOB_WORKSPACE_FILE ];
+	then
+		capture_stdout=$(rosws merge ${JOB_WORKSPACE_FILE} -r -y)
+	fi
+
+	# gathering repo info
 	repo_list=`rosws info --only=path,scmtype,version`
 	for line in $repo_list; do
 
 	  repo=(${line//,/ })
-	  path=${repo[0]} # branch
-	BRANCH_NAME=$1
+	  path=${repo[0]} 
 	  type=${repo[1]}
 	  version=${repo[2]}
 	  
+	  # check vcs type and inspect revision status
 	  if [ $type ]; then
 
 	    # git repository	
 	    if [ $type == "git" ]; then
+		
+	      # checking path first
+	      if [ ! -d ${path} ];
+	      then
+		      echo behind
+		      exit 0
+	      fi 
+
 	      cd $path
 	      if [ -z $version ]; then
 		version="HEAD"
