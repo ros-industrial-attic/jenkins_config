97523703dd2e29b145ce6007128cfb0f549fbc15
jenkins <jenkins@domain.org>
2013-07-26 17:27:43 -0500

    only builds by calling catkin_make

--- scripts/build_catkin_workspace.sh
+++ scripts/build_catkin_workspace.sh
@@ -4,25 +4,10 @@ source /opt/ros/groovy/setup.bash
 
 # variables
 SETUP_FILE=$WORKSPACE"/setup.bash"
-ROS_INSTALL_FILE=${JENKINS_HOME}/jenkins_config/workspaces/${JOB_NAME}.rosinstall
-
-# merging workspace file
-if [ -f $ROS_INSTALL_FILE ];
-then
-	echo "merging workspace file $ROS_INSTALL_FILE"
-	cd ${WORKSPACE}
-	rosws merge ${ROS_INSTALL_FILE} -r -y
-else
-	echo "workspace .rosinstall file not found, skip merge"
-fi
 
 #sourcing setup file
-echo "sourcing setup"
+echo "sourcing workspace setup script"
 source $SETUP_FILE
-
-#updating repositories
-cd $WORKSPACE
-rosws update --delete-changed-uris -v
 
 echo "============ Building catkin workspace ============"
 catkin_make clean
