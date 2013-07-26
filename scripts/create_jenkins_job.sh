e1fa1e8b49d8672d10ca7d950c0bf7e34718013c
jrgnicho <jrgnicho@gmail.com>
2013-07-26 11:28:19 -0500

    uses jenkins-cli.jar to create job

--- scripts/create_jenkins_job.sh
+++ scripts/create_jenkins_job.sh
@@ -8,14 +8,11 @@ CONFIG_FILE_PATH=${JENKINS_HOME}/jenkins_config/jobs/template_job/config.xml
 JOB_PATH=${JENKINS_HOME}/jobs/${JOB_NAME}
 JENKINS_CLI=${JENKINS_HOME}/jenkins-cli.jar
 
-# creating job directory
+# creating job
 if [ ! -d ${JOB_PATH} ];
-then
-	mkdir ${JOB_PATH}
-	cp ${CONFIG_FILE_PATH} ${JOB_PATH}
-
-	# reloading configuration
+then	
 	#wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+	java -jar ${JENKINS_CLI} -s http://localhost:8080 create-job ${JOB_NAME} < ${CONFIG_FILE_PATH}
 	java -jar ${JENKINS_CLI} -s http://localhost:8080 reload-configuration
 
 	echo "Created new jenkins job $JOB_NAME"
