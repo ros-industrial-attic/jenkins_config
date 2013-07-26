f78391b52161f06176f3d56aaaf4fd5d6199c264
jenkins <jenkins@domain.org>
2013-07-26 17:27:11 -0500

    merges .rosinstall file and calls rosws to update

--- scripts/update_catkin_workspace.sh
+++ scripts/update_catkin_workspace.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 # merge .rosinstall file and the calls rosws to update all repos
+
+# Setup
+source /opt/ros/groovy/setup.bash
+
+# variables
+SETUP_FILE=$WORKSPACE"/setup.bash"
+ROS_INSTALL_FILE=${JENKINS_HOME}/jenkins_config/workspaces/${JOB_NAME}.rosinstall
+
+# merging workspace file
+if [ -f $ROS_INSTALL_FILE ];
+then
+	echo "updating workspace file $ROS_INSTALL_FILE"
+	cd ${WORKSPACE}
+	rosws merge ${ROS_INSTALL_FILE} -r -y
+else
+	echo "workspace .rosinstall file not found, skip update"
+fi
+
+#sourcing setup file
+source $SETUP_FILE
+
+#updating repositories
+echo "updating repositories"
+rosws update --delete-changed-uris -v
+
