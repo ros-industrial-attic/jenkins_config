8109c4097c87b629e363adc9862b3e474662e16b
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2013-10-10 14:32:14 -0500

    commented code snippet for updating job configuration files

--- scripts/create_jenkins_job.sh
+++ scripts/create_jenkins_job.sh
@@ -8,14 +8,23 @@ CONFIG_FILE_PATH=${JENKINS_HOME}/jenkins_config/jobs/template_job/config.xml
 JOB_PATH=${JENKINS_HOME}/jobs/${JOB_NAME}
 JENKINS_CLI=${JENKINS_HOME}/jenkins-cli.jar
 
-# creating job
+
 if [ ! -d ${JOB_PATH} ];
-then	
+then
+	# creating job	
 	#wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 	java -jar ${JENKINS_CLI} -s http://localhost:8080 create-job ${JOB_NAME} < ${CONFIG_FILE_PATH}
 	java -jar ${JENKINS_CLI} -s http://localhost:8080 reload-configuration
 
 	echo "Created new jenkins job $JOB_NAME"
+else
+	# checking job current configuration
+	#config=$(java -jar ${JENKINS_CLI} -s http://localhost:8080 get-job ${JOB_NAME})
+
+	# updating job
+	#java -jar ${JENKINS_CLI} -s http://localhost:8080 update-job ${JOB_NAME} < ${CONFIG_FILE_PATH}
+	#java -jar ${JENKINS_CLI} -s http://localhost:8080 reload-configuration
+	#echo "Updated $JOB_NAME configuration file"
 fi
 
 
